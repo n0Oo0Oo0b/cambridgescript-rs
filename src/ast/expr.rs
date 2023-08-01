7f06f208d95d9ce25f3ac5d624db59259a858625
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-08-01 11:44:03 +0800

    Create AST nodes for expressions

---
+++ src/ast/expr.rs
@@ -0,0 +1,64 @@
+#[derive(Debug)]
+pub enum BinaryOperator {
+    LogicAnd,
+    LogicOr,
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Caret,
+    Equal,
+    NotEqual,
+    LessEqual,
+    GreaterEqual,
+    Less,
+    Greater,
+}
+
+#[derive(Debug)]
+pub enum UnaryOperator {
+    LogicNot,
+}
+
+pub enum Expr {
+    Binary(Box<BinaryExpr>),
+    Unary(Box<UnaryExpr>),
+    FunctionCall(Box<FunctionCallExpr>),
+    ArrayIndex(Box<ArrayIndexExpr>),
+    Literal(Literal),
+}
+
+pub struct BinaryExpr {
+    pub left: Expr,
+    pub operator: BinaryOperator,
+    pub right: Expr,
+}
+
+pub struct UnaryExpr {
+    pub operator: UnaryOperator,
+    pub right: Expr,
+}
+
+pub struct FunctionCallExpr {
+    pub function: Expr,
+    pub arguments: Vec<Expr>,
+}
+
+pub struct ArrayIndexExpr {
+    pub array: Expr,
+    pub indices: Vec<Expr>,
+}
+
+#[derive(Debug)]
+pub enum Literal {
+    Char(char),
+    String(String),
+    Integer(i64),
+    Real(f64),
+    Boolean(bool),
+}
+
+#[derive(Debug)]
+pub struct Identifier {
+    handle: u64,
+}
