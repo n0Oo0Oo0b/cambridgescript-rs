9f7e2e4f3f52f97f4fa706058b4f7cb99c2d26a6
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-08-31 15:41:15 +0800

    Update main.rs to use scanner::scan

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ use std::io;
 use std::io::prelude::*;
 
 mod scanner;
+mod ast;
+mod parser;
 
 fn main() {
     let mut input = String::new();
     let _ = io::stdin().read_to_string(&mut input);
 
-    for token in scanner::iter_tokens(input.as_str()) {
-        dbg!(&token);
+    let (tokens, errors) = scanner::scan(input.as_str());
+    for token in tokens {
+        dbg!(token);
+    }
+    for error in errors {
+        dbg!(error);
     }
 }
