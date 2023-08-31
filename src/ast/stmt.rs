c774673f8e8b865f95ed9822038984eb3cb64bc5
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-08-31 15:33:42 +0800

    Use enum attributes instead of separate structs for each statement type

--- src/ast/stmt.rs
+++ src/ast/stmt.rs
@@ -7,24 +7,75 @@ pub enum FileMode {
 }
 
 pub enum Stmt {
-    ProcedureDecl(ProcedureDeclStmt),
-    FunctionDecl(FunctionDeclStmt),
-    If(IfStmt),
-    CaseOf(Box<CaseOfStmt>),
-    ForLoop(ForLoopStmt),
-    RepeatUntil(RepeatUntilstmt),
-    While(WhileStmt),
-    VariableDecl(VariableDeclStmt),
-    ConstantDecl(ConstantDeclStmt),
-    Input(InputStmt),
-    Output(OutputStmt),
-    Return(ReturnStmt),
-    FileOpen(FileOpenStmt),
-    FileRead(FileReadStmt),
-    FileWrite(FileWriteStmt),
-    FileClose(FileCloseStmt),
-    Procedure(ProcedureStmt),
-    Assignment(AssignmentStmt),
+    ProcedureDecl {
+        name: Identifier,
+        params: Option<Vec<Parameter>>,
+        body: Block,
+    },
+    FunctionDecl {
+        name: Identifier,
+        params: Option<Vec<Parameter>>,
+        body: Block,
+    },
+    If {
+        condition: Expr,
+        then_branch: Block,
+        else_branch: Option<Block>,
+    },
+    CaseOf {
+        condition: Expr,
+        cases: Vec<(Expr, Stmt)>,
+        otherwise: Option<Box<Stmt>>,
+    },
+    ForLoop {
+        target: Expr,
+        start: Expr,
+        end: Expr,
+        step: Option<Expr>,
+        body: Block,
+    },
+    RepeatUntil {
+        body: Block,
+        condition: Expr,
+    },
+    While {
+        condition: Expr,
+        body: Block,
+    },
+    VariableDecl {
+        name: Identifier,
+        type_: Type,
+    },
+    ConstantDecl {
+        name: Identifier,
+        value: Literal,
+    },
+    Input(Vec<Expr>),
+    Output(Vec<Expr>),
+    Return(Expr),
+    FileOpen {
+        file: Literal,
+        mode: FileMode,
+    },
+    FileRead {
+        file: Literal,
+        target: Expr,
+    },
+    FileWrite {
+        file: Literal,
+        value: Expr,
+    },
+    FileClose {
+        file: Literal,
+    },
+    Procedure {
+        name: Identifier,
+        args: Option<Vec<Expr>>,
+    },
+    Assignment {
+        target: Expr,
+        value: Expr,
+    },
 }
 
 pub struct Block {
@@ -34,97 +85,4 @@ pub struct Block {
 pub struct Parameter {
     name: Identifier,
     type_: Type,
-}
-
-pub struct ProcedureDeclStmt {
-    name: Identifier,
-    params: Option<Vec<Parameter>>,
-    body: Block,
-}
-
-pub struct FunctionDeclStmt {
-    name: Identifier,
-    params: Option<Vec<Parameter>>,
-    body: Block,
-}
-
-pub struct IfStmt {
-    condition: Expr,
-    then_branch: Block,
-    else_branch: Option<Block>,
-}
-
-pub struct CaseOfStmt {
-    condition: Expr,
-    cases: Vec<(Expr, Stmt)>,
-    otherwise: Option<Stmt>,
-}
-
-pub struct ForLoopStmt {
-    target: Expr,
-    start: Expr,
-    end: Expr,
-    step: Option<Expr>,
-    body: Block,
-}
-
-pub struct RepeatUntilstmt {
-    body: Block,
-    condition: Expr,
-}
-
-pub struct WhileStmt {
-    condition: Expr,
-    body: Block,
-}
-
-pub struct VariableDeclStmt {
-    name: Identifier,
-    type_: Type,
-}
-
-pub struct ConstantDeclStmt {
-    name: Identifier,
-    value: Literal,
-}
-
-pub struct InputStmt {
-    target: Expr,
-}
-
-pub struct OutputStmt {
-    items: Vec<Expr>,
-}
-
-pub struct ReturnStmt {
-    value: Expr,
-}
-
-pub struct FileOpenStmt {
-    file: Literal,
-    mode: FileMode,
-}
-
-pub struct FileReadStmt {
-    file: Literal,
-    target: Expr,
-}
-
-pub struct FileWriteStmt {
-    file: Literal,
-    value: Expr,
-}
-
-pub struct FileCloseStmt {
-    file: Literal,
-}
-
-pub struct ProcedureStmt {
-    name: Identifier,
-    args: Option<Vec<Expr>>,
-}
-
-pub struct AssignmentStmt {
-    target: Expr,
-    value: Expr,
 }
