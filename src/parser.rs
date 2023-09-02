c4cae99d193e6ebe295ef1f344d5958746ecc66d
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-09-02 22:21:36 +0800

    Create empty parse methods

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
 use std::iter::Peekable;
+use crate::ast::*;
 use crate::scanner::Token;
 
 pub enum ParserError {
@@ -14,5 +15,51 @@ impl<T: IntoIterator<Item=Token>> From<T> for Parser<T> {
         Parser {
             tokens: value.into_iter().peekable(),
         }
+    }
+}
+
+impl<T: IntoIterator<Item=Token>> Parser<T> {
+    fn parse_block(&mut self) -> Vec<Stmt> {
+        todo!()
+    }
+
+    fn parse_stmt(&mut self) -> Vec<Stmt> {
+        todo!()
+    }
+
+    fn parse_expression(&mut self) -> Expr {
+        self.parse_logic_or()
+    }
+
+    fn parse_logic_or(&mut self) -> Expr {
+        todo!()
+    }
+
+    fn parse_logic_and(&mut self) -> Expr {
+        todo!()
+    }
+
+    fn parse_logic_not(&mut self) -> Expr {
+        todo!()
+    }
+
+    fn parse_comparison(&mut self) -> Expr {
+        todo!()
+    }
+
+    fn parse_term(&mut self) -> Expr {
+        todo!()
+    }
+
+    fn parse_factor(&mut self) -> Expr {
+        todo!()
+    }
+
+    fn parse_call(&mut self) -> Expr {
+        todo!()
+    }
+
+    fn parse_primary(&mut self) -> Expr {
+        todo!()
     }
 }
